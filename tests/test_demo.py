708289bad6481374f5fe1fceec9d0c7649b28f98
kesar <kesariben@gmail.com>
2024-07-08 14:27:15 +0300

    added test function

---
+++ tests/test_demo.py
@@ -0,0 +1,9 @@
+# tests/test_demo.py
+import unittest
+
+class TestDemo(unittest.TestCase):
+    def test_example(self):
+        self.assertEqual(1, 1)
+
+if __name__ == '__main__':
+    unittest.main()
